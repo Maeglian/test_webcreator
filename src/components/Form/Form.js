8e0759f4386f24644460dcccecd7d1a85a35fa99
Исаева Мария <isaeva-mariya@yandex.ru>
2020-02-27 18:27:36 +0300

    added custom checkbox

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,7 +7,7 @@ const Form = ({title, fields, groups, submit}) => {
             const additional = (group === "additional") ? " (не обязательно)" : "";
 
             return (
-                <div className="form-group col" key={name}>
+                <div className="col" key={name}>
                     <label htmlFor={name}>
                         {label + additional}
                     </label>
@@ -25,33 +25,30 @@ const Form = ({title, fields, groups, submit}) => {
     const layout = Object.entries(groups).map(([name, className]) => {
         const inputs = mapFields(fields, name);
 
-        const cn = (name === "main") ? " row row-cols-2" : "";
+        //const cn = (name === "main") ? " row row-cols-2" : "";
 
         return (
-            <div className={className + cn} >
+            <div className={className} >
                 {inputs}
             </div>
         )
     });
 
-    const checkbox = fields.map(({name, group, type, required, label}) => {
-        /*
+    const checkbox = fields.map(({name, type, required, label}) => {  
         if (type === "checkbox") {
+            const createMarkUp = () => ({
+                __html: label
+            });
+
             return (
-                <div className="row mb-cus-1">
-                    <div className="form-group col" key={name}>                    
-                    <div class="input-wrapper">
-                        <input className="form-control" id={name} name={name} type={type} required={required} />
-                        <span></span>
-                    </div>
-                    <label htmlFor={name}>
-                        {label}
-                    </label>
-                </div>  
+                <div className="form-row mb-cus-1">
+                    <div className="custom-control custom-checkbox col" key={name}>
+                        <input className="custom-control-input" id={name} name={name} type={type} required={required} />
+                        <label className="custom-control-label" htmlFor={name} dangerouslySetInnerHTML={createMarkUp()} />
+                    </div>  
                 </div>
             )
         }
-        */
 
         return null;
     })
@@ -61,7 +58,7 @@ const Form = ({title, fields, groups, submit}) => {
             <h2 class="mb-cus-1">
                 {title}
             </h2>
-            <div className="row mb-cus-1">
+            <div className="form-row mb-3">
                 {layout}
             </div>
             {checkbox}
